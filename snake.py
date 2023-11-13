93d24524fe5add36b0df59a9eb91684b59ad1484
Freddy <72482485+fmckenzie05@users.noreply.github.com>
2023-11-13 12:26:54 -0800

    Update snake.py
    update

--- snake.py
+++ snake.py
@@ -1,40 +1,38 @@
-"""Snake, classic arcade game.
-
-Exercises
-
-1. How do you make the snake faster or slower?
-2. How can you make the snake go around the edges?
-3. How would you move the food?
-4. Change the snake to respond to mouse clicks.
-"""
-
 from random import randrange
 from turtle import *
-
 from freegames import square, vector
 
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
+score = 0  # Initialize score
 
+def wrap(value, min, max):
+    """Wrap value around min and max."""
+    if value > max:
+        return min
+    if value < min:
+        return max
+    return value
 
 def change(x, y):
     """Change snake direction."""
     aim.x = x
     aim.y = y
 
-
 def inside(head):
-    """Return True if head inside boundaries."""
-    return -200 < head.x < 190 and -200 < head.y < 190
-
+    """Wrap snake head around edges if it goes out of boundaries."""
+    head.x = wrap(head.x, -200, 190)
+    head.y = wrap(head.y, -200, 190)
+    return True
 
 def move():
-    """Move snake forward one segment."""
+    """Move snake forward one segment and move food randomly."""
+    global score
     head = snake[-1].copy()
     head.move(aim)
 
-    if not inside(head) or head in snake:
+    if head in snake:
         square(head.x, head.y, 9, 'red')
         update()
         return
@@ -42,7 +40,8 @@ def move():
     snake.append(head)
 
     if head == food:
-        print('Snake:', len(snake))
+        score += 1  # Increase score
+        display_score()  # Update score display
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
     else:
@@ -57,14 +56,42 @@ def move():
     update()
     ontimer(move, 100)
 
+def change_direction(x, y):
+    """Change direction based on mouse click position relative to snake's head."""
+    head = snake[-1]
+    dx = x - head.x
+    dy = y - head.y
+
+    if abs(dx) > abs(dy):
+        # Horizontal movement takes precedence if the click is more horizontal than vertical
+        if dx > 0:
+            change(10, 0)  # Move right
+        else:
+            change(-10, 0)  # Move left
+    else:
+        # Vertical movement otherwise
+        if dy > 0:
+            change(0, 10)  # Move up
+        else:
+            change(0, -10)  # Move down
+
+def display_score():
+    """Display the current score on the screen."""
+    score_turtle.clear()
+    score_turtle.write(f"Score: {score}", align="center", font=("Arial", 14, "normal"))
 
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 listen()
-onkey(lambda: change(10, 0), 'Right')
-onkey(lambda: change(-10, 0), 'Left')
-onkey(lambda: change(0, 10), 'Up')
-onkey(lambda: change(0, -10), 'Down')
+onscreenclick(change_direction)  # Change to mouse click
+
+# Score display setup
+score_turtle = Turtle(visible=False)
+score_turtle.penup()
+score_turtle.goto(0, 160)
+score_turtle.color("black")
+display_score()  # Initial score display
+
 move()
 done()
